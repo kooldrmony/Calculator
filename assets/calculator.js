7e86e5e2f265db6b91a81fc33e2da823ee8f992e
kooldrmony <derrickreyes.23@gmail.com>
2019-02-06 22:00:45 -0700

    added newe javscript functions

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,45 +1,56 @@
-function number1() {
-	document.getElementById("screen").innerHTML = "1";}
+function insert(num){
+	document.form.view.value = document.form.view.value+num
+}
 
-function number2() {
-  		document.getElementById("screen").innerHTML = "2";}
+function equal(){
+	const exp = document.form.view.value;
+	if(exp){
+		document.form.view.value = eval(exp)
+	}
+}
 
-  		function number3() {
-  		document.getElementById("screen").innerHTML = "3";}
+// function number1() {
+// 	document.getElementById("screen").innerHTML = "1";}
 
-  		function number4() {
-  		document.getElementById("screen").innerHTML = "4";}
+// function number2() {
+//   		document.getElementById("screen").innerHTML = "2";}
 
-  		function number5() {
-  		document.getElementById("screen").innerHTML = "5";}
+//   		function number3() {
+//   		document.getElementById("screen").innerHTML = "3";}
 
-  		function number6() {
-  		document.getElementById("screen").innerHTML = "6";}
+//   		function number4() {
+//   		document.getElementById("screen").innerHTML = "4";}
 
-  		function number7() {
-  		document.getElementById("screen").innerHTML = "7";}
+//   		function number5() {
+//   		document.getElementById("screen").innerHTML = "5";}
 
-  		function number8() {
-  		document.getElementById("screen").innerHTML = "8";}
+//   		function number6() {
+//   		document.getElementById("screen").innerHTML = "6";}
 
-  		function number9() {
-  		document.getElementById("screen").innerHTML = "9";}
+//   		function number7() {
+//   		document.getElementById("screen").innerHTML = "7";}
 
-  		function number0() {
-  		document.getElementById("screen").innerHTML = "0";}
+//   		function number8() {
+//   		document.getElementById("screen").innerHTML = "8";}
 
-  		function plus() {
-  		document.getElementById("screen").innerHTML = "+";}
+//   		function number9() {
+//   		document.getElementById("screen").innerHTML = "9";}
 
-  		function minus() {
-  		document.getElementById("screen").innerHTML = "-";}
+//   		function number0() {
+//   		document.getElementById("screen").innerHTML = "0";}
 
-  		function division() {
-  		document.getElementById("screen").innerHTML = "/";}
+//   		function plus() {
+//   		document.getElementById("screen").innerHTML = "+";}
 
-  		function multiply() {
-  		document.getElementById("screen").innerHTML = "*";}
+//   		function minus() {
+//   		document.getElementById("screen").innerHTML = "-";}
 
-  		function equal() {
-  		document.getElementById("screen").innerHTML = "=";}
+//   		function division() {
+//   		document.getElementById("screen").innerHTML = "/";}
+
+//   		function multiply() {
+//   		document.getElementById("screen").innerHTML = "*";}
+
+//   		function equal() {
+//   		document.getElementById("screen").innerHTML = "=";}
 
